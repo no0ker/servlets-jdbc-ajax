446b525fa29c951ca47e89d2fb1bb920ab955c92
rush0714 <pukanGi9>
2016-10-03 19:55:45 +0300

    comment

--- src/main/java/finder/ajax_entities/ResultTable.java
+++ src/main/java/finder/ajax_entities/ResultTable.java
@@ -1,32 +1,10 @@
 package finder.ajax_entities;
 
-import finder.Entity.Car;
-import finder.Entity.User;
-
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class ResultTable {
     private Set<Row> rows = new HashSet<Row>();
-
-    public ResultTable(List<User> users) {
-        for (User user : users) {
-            if (user.getCar() != null && !user.getCar().isEmpty()) {
-                for (Car car : user.getCar()) {
-                    rows.add(new Row(user.getFirstName(),
-                            user.getSecondName(),
-                            user.getCity().getName(),
-                            car.getColor()));
-                }
-            } else {
-                rows.add(new Row(user.getFirstName(),
-                        user.getSecondName(),
-                        user.getCity().getName(),
-                        null));
-            }
-        }
-    }
 
     public Set<Row> getRows() {
         return rows;
